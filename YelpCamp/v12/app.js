4cb29a3f470ca2ecb6afc55e6285c09639ab74b7
theIceman <kushagrakumar34@gmail.com>
2019-09-13 22:44:25 +0530

    Modified it to listen on port 3000

--- YelpCamp/v12/app.js
+++ YelpCamp/v12/app.js
@@ -48,6 +48,6 @@ app.use(indexRoutes);
 app.use(commentRoutes);
 app.use(campgroundRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, function(){
     console.log("The Yelp Camp Server Has Started");
 });
